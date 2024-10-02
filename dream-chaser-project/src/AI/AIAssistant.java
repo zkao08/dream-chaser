a62f424a89b268f5b25b2a300e3bf1d29e4bab17
zkao08 <zachkao@hotmail.com>
2024-10-01 19:26:39 -0500

    Added empty java project

---
+++ dream-chaser-project/src/AI/AIAssistant.java
@@ -0,0 +1,10 @@
+package AI;
+
+/**
+ * The AI Assistant class will integrate genAI API to provide the user
+ * with assistance in creating and managing goals
+ */
+public class AIAssistant
+{
+	
+}
