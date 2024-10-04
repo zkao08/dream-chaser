dd6a3a5c6443ba3b756a143bfe4d4e2e5c56f878
mai1309 <atismailisa@gmail.com>
2024-10-04 13:18:11 -0500

    Create creation.java
    Class for Goal Creation

---
+++ dream-chaser-project/src/GoalCreation/creation.java
@@ -0,0 +1,64 @@
+package GoalCreation;
+
+/**
+ * The creation class will help create the goals that want to be 	
+ * reached by the users. It represents a personal or project goal 	
+ * with a title, descrption, deadline, and completion status. 
+ */
+public class creation
+{
+	
+}
+
+//Pseudocode
+Class creation
+
+ATTRIBUTES
+    goalName: String			//name of the goal
+    description: String			//descrption of goal
+    targetDate: Date			//date goal intended to be reached
+    isCompleted: Boolean		//if the goal is completed or not
+
+METHODS
+	//sets all the goal information from creation of object
+    constructor(goalName: String, description: String, targetDate: Date)
+        this.goalName = goalName
+        this.description = description
+        this.targetDate = targetDate
+        this.isCompleted = false
+
+    //getter functions
+    getGoalName(): String
+        return this.goalName
+
+    getDescription(): String
+        return this.description
+
+    getTargetDate(): Date
+        return this.targetDate
+
+    getIsCompleted(): Boolean
+        return this.isCompleted
+
+    //setter functions
+    setGoalName(goalName: String)
+        this.goalName = goalName
+
+    setDescription(description: String)
+        this.description = description
+
+    setTargetDate(targetDate: Date)
+        this.targetDate = targetDate
+
+    //marks goal completed
+    markComplete()
+        this.isCompleted = true
+
+    //displays goal information
+    displayGoal()
+        PRINT "Goal: " + this.goalName
+        PRINT "Description: " + this.description
+        PRINT "Target Date: " + this.targetDate
+        PRINT "Completed: " + (this.isCompleted ? "Yes" : "No")
+        
+        
